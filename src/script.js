f6caf3ed3bf4216b57d5e0fa53fdfe1eaa37a892
rakaxx <rakaikbarsetyadireza@gmail.com>
2025-03-26 00:19:10 +0700

    New UI v2.0 Home and About Desktop

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const menuBurger = document.getElementById('burger-menu');
-const menuList = document.getElementById('menu-list');
-
-menuBurger.addEventListener('click', () => {
-    menuList.classList.toggle("hidden");
-});
